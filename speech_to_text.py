b349473daeebd66a32fb56a4034cca6f15cb093d
ShivaRaj-45 <23eg105t41@anurag.edu.in>
2025-08-23 16:49:26 +0530

    Update speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,15 +1,8 @@
 import speech_recognition as sr
-
-# Initialize recognizer
 recognizer = sr.Recognizer()
-
-# Use microphone as source
 with sr.Microphone() as source:
     print("Speak now: ")
-    # Listen to audio from the microphone
     audio = recognizer.listen(source)
-
-# Try to recognize the speech using Google Speech Recognition
 try:
     text = recognizer.recognize_google(audio)
     print("You said:", text)
